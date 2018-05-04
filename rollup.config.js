6c6cd774812ff3c8f457c1bd41cdb7eee36e5353
Sveinn Thorarinsson <sveinn.thorarinsson@gmail.com>
2018-05-03 23:02:59 -0700

    changing build step

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,18 @@ import sourceMaps from 'rollup-plugin-sourcemaps';
 import babel from 'rollup-plugin-babel';
 import camelCase from 'lodash.camelcase';
 
-const pkg = require('./package.json');
 const libraryName = 'routing-number-validator';
 
 export default {
   input: `src/${libraryName}.js`,
   output: [
     {
-      file: 'pkg.main',
+      file: 'dist/routing-number-validator.umd.js',
       name: camelCase(libraryName),
       format: 'umd'
     },
     {
-      file: 'pkg.module',
+      file: 'dist/routing-number-validator.es5.js',
       format: 'es'
     }
   ],
